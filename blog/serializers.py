fa4f40e2879a12dccdc22f45065f800978983f9f
speedssster <lastprogamer0@gmail.com>
2021-11-02 16:41:44 +0330

    Article update flow part2 (done)

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework.fields import CharField
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import ModelSerializer
 from .models import Article
 
 
 class ArticleSerializer(ModelSerializer):
-    status = CharField(source="get_status_display")
+    status = CharField(source="get_status_display", required=False)
 
     class Meta:
         model = Article
         fields = (
             "uuid",
             "author",
+            "slug_title",
             "created_at",
             "updated_at",
             "likes",
@@ -19,13 +20,13 @@ class ArticleSerializer(ModelSerializer):
             "status",
             # ---
             "title",
-            "slug_title",
             "content",
             "image",
         )
         read_only_fields = (
             "uuid",
             "author",
+            "slug_title",
             "created_at",
             "updated_at",
             "likes",
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
+from rest_framework import response
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import (
@@ -7,9 +8,10 @@ from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView,
     CreateAPIView,
 )
+from django.forms.models import model_to_dict
 
 
-from authentication.permissions import OwnerAndAdminOrReadOnly
+from authentication.permissions import OwnerAndAdmin, OwnerAndAdminOrReadOnly
 from .serializers import ArticleSerializer
 from .models import Article
 
@@ -47,20 +49,82 @@ class ArticleRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 
     def perform_update(self, serializer):
         admin = self.request.user.is_superuser
+        status = serializer.instance.status
 
-        if (serializer.instance.status == Article.PENDING) and (not admin):
+        if (status == Article.PENDING) and (not admin):
             return Response(
                 {
                     "error": "article-pending",
-                    "message": "You can't update a pending article",
+                    "message": "You can't update a pending article.",
                     "detail": "Article is already pending.",
                 },
                 status=status.HTTP_403_FORBIDDEN,
             )
-        elif serializer.instance.status == Article.PUBLISHED:
-            pass
+        elif status == Article.PUBLISHED:
+            if serializer.instance.clone:
+                return Response(
+                    {
+                        "error": "article-pending",
+                        "message": "You have to wait for previous changes.",
+                        "detail": "Article is already submited for review.",
+                    },
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+            data = serializer.validated_data
+            data["status"] = Article.PENDING
+            Article.objects.create(**data)
         else:
             serializer.save()
+
+
+class ArticlePublishAPIView(CreateAPIView):
+    """Change Article Status to PUBLISHED"""
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [OwnerAndAdmin]
+    lookup_field = "uuid"
+
+    def create(self, request, *args, **kwargs):
+        obj = self.get_object()
+        admin = request.user.is_superuser
+        # Only NON-Admin Authors can set PENDING status
+        # (Asking for review from admins) ->
+        if (not admin) and obj.status == Article.DRAFT:
+            # if user is not admin then it must be auhtor
+            obj.status = Article.PENDING
+            obj.save()
+        # Only Admin can publish articles with 'pending' status ->
+        elif admin and obj.status == Article.PENDING:
+            # By using clone mechanism, published articles remain
+            # intact untill their clone get published.
+            if original := obj.original:
+                # replacing original article with clone data
+                for field, value in model_to_dict(obj).items():
+                    setattr(original, field, value)
+                original.save()
+                # removing temp clone
+                obj.delete()
+            else:
+                obj.status = Article.PUBLISHED
+                obj.save()
+        else:
+            detail = (
+                "You dont have permission to publish a pending article."
+                if obj.status == 1
+                else "Article is already published"
+            )
+            # None of above case happends so its a bad request
+            return Response(
+                {
+                    "error": "article-publish",
+                    "message": "You have to be Admin or Article must be draft.",
+                    "detail": detail,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        # The happy ending! ->
+        return Response(status=status.HTTP_200_OK)
 
 
 class ArticleLikeAPIView(CreateAPIView):
@@ -73,7 +137,7 @@ class ArticleLikeAPIView(CreateAPIView):
     def create(self, request, *args, **kwargs):
         article = self.get_object()
         user = request.user
-        if user in article.likes.all():  # TODO: Fix Query
+        if Article.objects.filter(uuid=article.uuid, likes__in=[user]).exists():
             article.likes.remove(user)
         else:
             article.likes.add(user)
@@ -97,8 +161,9 @@ class ArticleBookmarkAPIView(CreateAPIView):
         return Response(status=status.HTTP_200_OK)
 
 
-# Increase an Article's share
 class ArticleIncreaseShareAPIView(CreateAPIView):
+    """Increase Article Share Count"""
+
     permission_classes = [IsAuthenticated]
     queryset = Article.objects.all()
     lookup_field = "uuid"
